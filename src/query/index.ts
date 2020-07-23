863697d6a32d4baa7ed68825d81068108c849a3a
Alan Poulain <contact@alanpoulain.eu>
2020-07-23 22:54:21 +0200

    Website generation with Couscous

--- src/query/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as GeocodeQuery } from "query/GeocodeQuery";
-export * from "query/GeocodeQuery";
-export { default as ReverseQuery } from "query/ReverseQuery";
-export * from "query/ReverseQuery";
--- src/types.ts
+++
@@ -1,8 +0,0 @@
-export type PartialSome<
-  O,
-  P extends keyof O,
-  Q = Omit<O, P>,
-  R = { [K in P]?: O[P] }
-> = Q & R;
-
-export type Box = [number, number, number, number];
